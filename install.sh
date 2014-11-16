534659be7e36c29e7ce325cfdef4c45340621ee0
MouseMs <mousems.kuo@gmail.com>
2014-11-16 20:03:29 +0800

    fix

--- install.sh
+++ install.sh
@@ -1,38 +1,44 @@
-echo -e "before install , please make sure network and open-ssh server already setup.\n" &&
+echo -e "before install , please make sure network and open-ssh server already setup.\n" 
 
-apt-get install -y git &&
-git clone https://github.com/mousems/ntuvotev2.git && 
+# git and tasksel
+apt-get install -y git 
+apt-get install -y tasksel 
+apt-get update 
+#clone
+git clone https://github.com/mousems/ntuvotev2.git 
 
-tasksel install lamp-server &&
+#install LAMP
+tasksel install lamp-server 
+apt-get update 
 
 
-apt-get update &&
+#config apache
+a2enmod rewrite 
+a2enmod ssl 
+mkdir /etc/apache2/ssl 
+openssl req -x509 -nodes -days 365000 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt 
+mv /etc/apache2/apache2.conf /etc/apache2/apache2.conf.bak 
+cp apache2.conf /etc/apache2/ 
+cp default-ssl.conf /etc/apache2/sites-enabled/default-ssl.conf 
+cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf 
+chown root:root /etc/apache2/sites-enabled/default-ssl.conf 
+chmod 644 /etc/apache2/sites-enabled/default-ssl.conf 
+sudo apt-get install -y php5-json
+service apache2 restart 
 
+# app file
+rm -rf /var/www/* 
+mv ntuvotev2/* /var/www 
+cp ./ntuvotev2/.htaccess /var/www
+mv config.php /var/www/application/config 
+mv Result.sh /var/log/NTUticket 
 
-a2enmod ssl &&
-mkdir /etc/apache2/ssl &&
-openssl req -x509 -nodes -days 365000 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt &&
-a2enmod rewrite &&
-mv /etc/apache2/apache2.conf /etc/apache2/apache2.conf.bak &&
-cp apache2.conf /etc/apache2/ &&
-cp default-ssl.conf /etc/apache2/sites-enabled/default-ssl.conf &&
-cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf &&
-chown root:root /etc/apache2/sites-enabled/default-ssl.conf &&
-chmod 644 /etc/apache2/sites-enabled/default-ssl.conf &&
-
-service apache2 restart &&
-
-rm -rf /var/www/* &&
-mv ntuvotev2/* /var/www &&
-mv config.php /var/www/application/config &&
-mv Result.sh /var/log/NTUticket &&
-cp ./ntuvotev2/.htaccess /var/www &&
-mkdir /var/log/NTUticket &&
-
-cd /var/log/NTUticket && git init && chmod 777 ./ && chmod -R 777 .git
-
-sudo apt-get install -y php5-json &&
-
+#ticket storage
+mkdir /var/log/NTUticket 
+cd /var/log/NTUticket 
+git init 
+chmod 777 ./ 
+chmod -R 777 .git
 
 
 echo -e "\n\n\ninstall complete \n    1.Please connect to MySQL and import SQL file. 'ntuvote.sql'\n    2.Edit /var/www/application/config/databases.php for MySQL settings.\n    3.Edit /var/www/application/config/config.php for 'URL'.\n    4.visit website , administrator account: ntuvote/ntuvote\!@#$%\n\n\n\n\n"
